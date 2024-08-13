f79ce9bab72d3c8a3d3fc6f81ca743761e1d6deb
KaranSapra124 <sapra7112@gmail.com>
2024-08-13 10:43:52 +0530

    Bcrypt Integrated (13-08-2024)

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -1,14 +1,18 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const userModel = require("../Model/UserModel");
+const bcrypt = require("bcrypt");
+// VIKAS123 // ejlrjfjrfkerkfmicnr_eicnrinci!knknkonid@jk#cikenci@
 
 module.exports.userSignUp = async (req, res) => {
-  console.log(req.body);
+  const { password } = req.body;
+
+  // console.log(hashPass);
   const newData = await userModel.create({
     ...req.body,
+    password: await bcrypt.hash(password, 5),
     phnNum: +req.body.phnNum,
   });
-  return res.status(200).send({ message: "You have logged in!", newData });
+  return res.status(200).send({ message: "Your Account is created!", newData });
 };
 
 module.exports.getAllBlogs = (req, res) => {
